108f1a5e77e3e6fd6e53096f589f4ce2779d4fbd
kumaravel <velk9111@gmail.com>
2025-10-04 15:23:25 +0530

    Reset index and apply gitignore

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiWithoutAws.Controllers
+{
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public IActionResult Get()
+        {
+            var message = Environment.GetEnvironmentVariable("MY_MESSAGE")
+                          ?? "Hello from default!";
+            var name = Environment.GetEnvironmentVariable("MY_NAME")
+                       ?? "Default Name";
+            return Ok(new { Message = message, Name = name });
+        }
+    }
+
+}
---
+++ Dockerfile
@@ -0,0 +1,16 @@
+﻿# Step 1: Build Stage
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+COPY . .
+RUN dotnet restore
+RUN dotnet publish -c Release -o /app
+
+# Step 2: Runtime Stage
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /app
+COPY --from=build /app .
+ENTRYPOINT ["dotnet", "WebApiWithoutAws.dll"]
+
+# environment variables 
+ENV MY_MESSAGE="Hello from Docker ENV!"
+ENV MY_NAME="KUMAR"
